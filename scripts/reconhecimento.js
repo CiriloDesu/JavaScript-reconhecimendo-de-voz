2a23bb2a44fd8e36be45cb83afffa8e0d5871d43
Jeferson <32491548+CiriloDesu@users.noreply.github.com>
2023-05-24 11:54:13 -0300

    pt-br dnv

--- scripts/reconhecimento.js
+++ scripts/reconhecimento.js
@@ -3,7 +3,7 @@ const elementoChute = document.getElementById('chute')
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
-recognition.lang = 'ja-JP'
+recognition.lang = 'pt-BR'
 recognition.start()
 
 recognition.addEventListener('result', onSpeak)
